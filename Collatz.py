b35eeea5fd5ac5262100279a94d1c74c085709b1
Silvio622 <sdunst1@eircom.net>
2020-02-17 14:55:57 +0000

    Python File

--- Collatz.py
+++ Collatz.py
@@ -3,17 +3,26 @@
 # At each step calculate the next value by taking the current value and, if it is even, divide it by two, 
 # but if it is odd, multiply it by three and add one. Have the program end if the current value is one.
 
-currentvalue = int(input("Please Enter a positive Integer: ")) # convert a Input to an Integer
-m = 2 # If you divide a number by 2 and it gives a remainder of 0 then it is known as even number, otherwise an odd number.
-nextvalue = currentvalue
+import time # import time function block for troubleshooting to slow down the while loop
 
-while currentvalue == 1:
-    
-    nextvalue = nextvalue - currentvalue
-    if nextvalue % m == 0: # if you have a remainder of 0 it is true it is a even number
-        nextvalue = nextvalue / 2
-    else:
-        nextvalue = (nextvalue * 3) + 1
-        print(nextvalue)
-if currentvalue <= 0: 
+UserInput = int(input("Please Enter a positive Integer: ")) # convert a Input to an Integer
+StartValue = 30 # create a variable for start the equation
+ResultList = [] # a list variable
+
+if UserInput > 0: # the user input must be a positive number
+    CurrentValue = StartValue - UserInput # create a variable currentvalue for the process
+   
+    while CurrentValue > 1: # the while loops so long the currentvalue is greater then 1 
+  
+        if CurrentValue % 2 == 0: # If you divide a number by 2 and it gives a remainder of 0 then it is known as even number, otherwise an odd number.
+             CurrentValue = CurrentValue / 2 # even number devide the currentvalue by 2
+        else:
+            CurrentValue = (CurrentValue * 3) + 1 # odd number multiply currentvalue by 3 and add 1       
+      
+        ResultList.append(int(CurrentValue)) # writes a new entry every time to the end of the list (Append object) as currentvalue
+        time.sleep(0.5)  # use timer to slow down the while loop for troubleshooting 
+        print(CurrentValue) #use print out of the currentvalue for troubleshooting
+    print(ResultList) # print out all the currentvalues in a list variable Resultlist
+else: # when the user input is a negative number
   print("The number you entered is not a positive Integer")
+
