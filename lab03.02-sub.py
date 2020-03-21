ef7e65dd2e25187e0e0d15c91ee23b8b0856d8a5
Silvio622 <sdunst1@eircom.net>
2020-03-21 17:34:04 +0000

    Lab 04 Files

--- lab03.02-sub.py
+++
@@ -1,9 +0,0 @@
-# Program to subtract one number from another
-
-# inputs reads in a string so we need to convert it into to a integer (int)
-# so we can perform mathematical operations
-
-x = int(input("Enter first number: "))# converts the input to a Interger
-y = int(input("Enter second number: "))# converts the input to a Interger
-answer = x-y
-print("{} minus {} is {}".format(x, y, answer))
